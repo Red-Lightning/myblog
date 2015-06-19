2f8fdad0a65a30e1fa1bbde8e37f255fc07ade70
Red-Lightning <flame_kuro@hotmail.com>
2015-06-19 18:45:08 +0530

    create models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = "This is some sample text for our awesome new Ruby blog"
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+end
